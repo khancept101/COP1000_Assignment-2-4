0cb080fbf65691bd4e545e9d81a955b30c01fbb7
khancept101 <ksumayyah2000@gmail.com>
2025-04-18 21:31:57 -0400

    Added logic for salary, dependents, and calculating the output

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-# input statements
-salary = float(input())
-numDependents = float(input())
+# input statements (adding prompt strings for better user readability)
+salary = float(input("Enter weekly salary: "))
+numDependents = float(input("Enter number of dependents: "))
 
 # calculate taxes here
+stateTax = salary * 0.065
+federalTax = salary * 0.28
+dependentDeduction = salary * 0.025 * numDependents
+totalWithholding = stateTax + federalTax + dependentDeduction
+takeHomePay = salary - totalWithholding
 
 # output statements
+print("State Tax: $" + str(stateTax))
+print("Federal Tax: $" + str(federalTax))
+print("Dependents: $" + str(dependentDeduction))
 print("Salary: $" + str(salary))
-print("Take Home Pay: $" + str(takeHomePay))
+print("Take-Home Pay: $" + str(takeHomePay))
